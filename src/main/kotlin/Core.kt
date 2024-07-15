695a92bf090f062fe8214dc3523148089379884e
Neo <c.min.tseng@gmail.com>
2024-07-16 07:35:04 +0800

    export Steganography Encoder/Decoder to fun

--- src/main/kotlin/Core.kt
+++ src/main/kotlin/Core.kt
@@ -26,3 +26,36 @@ public fun getBufferedImage(file: File): BufferedImage? {
 
 public  fun decodeMessage(message: List<Byte>, password: String) =
     transformBits(message, password).joinToString("").chunked(8).map { it.toInt(2).toChar() }.joinToString("")
+
+public fun steganographyEncoder(image: BufferedImage, message_with_pwd_and_end_bits:  List<Byte>): BufferedImage {
+    val copyImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
+    var index = 0
+    start@ for (y in 0 until copyImage.height) {
+        for (x in 0 until copyImage.width) {
+            if (index == message_with_pwd_and_end_bits.size) break@start
+            val bit = message_with_pwd_and_end_bits[index++].toInt()
+            val color = Color(copyImage.getRGB(x, y)).let {
+                Color(
+                    it.red,
+                    it.green,
+                    it.blue.and(254).or(bit)
+                )
+            }
+            copyImage.setRGB(x, y, color.rgb)
+        }
+    }
+    return copyImage
+}
+
+public fun steganographyDecoder(image: BufferedImage,password: String) :String {
+    val message = mutableListOf<Byte>()
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
+            message.add((Color(image.getRGB(x, y)).blue % 2).toByte())
+            if (message.size >= END.size && message.size % 8 == 0 && message.takeLast(END.size) == END) {
+                return "Message:\n" + decodeMessage(message.dropLast(END.size), password)
+            }
+        }
+    }
+    return "No message was hidden!"
+}
